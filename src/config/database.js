7aa1d0a6aaa94c9839e32b0ccf0e4de7d46c3ce1
unknown <alvesp2001@gmail.com>
2020-08-29 16:34:29 -0300

    aula

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     dialect: 'mysql',
-    host: 'localhost',
-    username: 'root',
-    password: '1234',
+    host: 'ls-205cf2fd667af36d71c4742e1d567c5a112ceb4e.ceefo25bexkd.us-east-1.rds.amazonaws.com',//'localhost',
+    username: 'dbmasteruser',//'root',
+    password: '6D:2WhKVtKo~|R,2R?#|E5k+H<6Un{jH',//'1234',
     database: 'dbfms',
     define: {
         timestamps: true,
